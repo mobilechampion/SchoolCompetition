a633b46b251a5bad85ea763a3ec3abaac5346ff7
gold <gold@golds-iMac.local>
2015-03-26 15:49:38 +0800

    Initial Commit

---
+++ SchoolCompetition/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SchoolCompetition
+//
+//  Created by gold on 3/26/15.
+//  Copyright (c) 2015 gold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SchoolCompetition/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SchoolCompetition
+//
+//  Created by gold on 3/26/15.
+//  Copyright (c) 2015 gold. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
